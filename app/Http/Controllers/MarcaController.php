e5712ba018e0617c4402f3312d3b851c93ea1e96
Davi Carminati <davicarminatip@gmail.com>
2026-07-01 18:40:57 -0300

    t0003

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,14 +7,18 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca){
+        $this->marca = $marca;
+    }    
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-        return 'index';
-
+        //$marcas = Marca::all();
+        $marca = $this->marca->all();
+        
+        return response()->json($marca, 200);
     }
 
     /**
@@ -29,16 +33,32 @@ class MarcaController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        return json_encode($request);
+    {  
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens', 'public');
+
+        $marca = $this->marca->create([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn
+        ]);
+        
+        return response()->json($marca, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        return 'get';
+        $marca = $this->marca->find($id);
+        if($marca === null) {
+            return response()->json(['erro' => 'Recurso pesquisado nao existe'], 404) ;        
+        }
+        return response()->json($marca, 200);
     }
 
     /**
@@ -52,16 +72,58 @@ class MarcaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+
+       $marca = $this->marca->find($id);
+        if($marca === null) {
+            return response()->json(['erro' => 'Impossivel relaziar a atualizaçao. Recurso nao encrotado'],404);
+        }
+
+        if($request->method() === 'PATCH'){
+
+            $regrasDinamicas = array();
+
+            $teste = '';
+        
+            foreach($marca->rules() as $input => $regra){
+
+                if(array_key_exists($input, $request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            dd($regrasDinamicas);
+
+            $request->validate($regrasDinamicas->rules(), $marca->feedback());
+        }
+        else{
+
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens', 'public');
+
+        $marca->update([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn
+        ]);
+
+       return response()->json($marca, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca = $this->marca->find($id);
+
+        if($marca === null){
+            return response()->json(['erro' => 'Voce nao pode apagar um registro q nao existe meu caro'],404);
+        }
+        
+        $marca->delete();
+        return response()->json(['msg' => 'A marca foi removida com sucesso'], 200);
     }
 }
