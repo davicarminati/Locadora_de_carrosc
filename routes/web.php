3d8b0b2e475da142786910c0e05669755c4b0f00
Davi Carminati <davicarminatip@gmail.com>
2026-07-30 18:20:32 -0300

    t0019

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 Route::get('api', function () {
     return view('api');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
